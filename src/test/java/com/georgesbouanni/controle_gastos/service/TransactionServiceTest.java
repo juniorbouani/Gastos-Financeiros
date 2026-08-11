d9fcede0121372c93483463b2dacd91ee196e713
Georges <juniorbouani@gmail.com>
2026-08-11 12:39:23 -0300

    salvar, delecao e atualizacao do TserviceTest

--- src/test/java/com/georgesbouanni/controle_gastos/service/TransactionServiceTest.java
+++ src/test/java/com/georgesbouanni/controle_gastos/service/TransactionServiceTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.times;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
@@ -49,4 +50,59 @@ public class TransactionServiceTest {
 
     }
 
+    @Test
+    void deveBuscarTransacaoPorId() {
+        Transaction transaction = new Transaction(
+                null,
+                "Mercado",
+                new BigDecimal("150.50"),
+                LocalDate.of(2026,7,20),
+                TransactionType.EXPENSE,
+                "Alimentação"
+        );
+        transaction.setId("abc123");
+
+        when(repository.findById("abc123")).thenReturn(Optional.of(transaction));
+
+        Optional<Transaction> resultado = service.findById("abc123");
+
+        Assertions.assertTrue(resultado.isPresent());
+        Assertions.assertEquals("Mercado", resultado.get().getDescription());
+    }
+
+    @Test
+    void deveRetornarVazioQuandoNãoTiverID() {
+        when(repository.findById("id-invalido")).thenReturn(Optional.empty());
+
+        Optional<Transaction> resultado = service.findById("id-invalido");
+
+        Assertions.assertTrue(resultado.isEmpty());
+    }
+
+    @Test
+    void deveSalvarTransacao() {
+        Transaction transaction = new Transaction(
+                null,
+                "Mercado",
+                new BigDecimal("150.50"),
+                LocalDate.of(2026, 7, 20),
+                TransactionType.EXPENSE,
+                "Alimentação"
+        );
+
+        when(repository.save(transaction)).thenReturn(transaction);
+
+        Transaction resultado = service.save(transaction);
+
+        Assertions.assertNotNull(resultado);
+        Assertions.assertEquals("Mercado", resultado.getDescription());
+        Mockito.verify(repository, times(1)).save(transaction);
+    }
+
+    @Test
+    void deveDeletarTransacao() {
+        service.delete("abc123");
+
+        Mockito.verify(repository, times(1)).deleteById("abc123");
+    }
 }
